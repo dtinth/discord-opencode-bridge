42a8610e57701ce16abbcfb71daded1e60867a86
dtinth-ampere-devbox <dtinth@spacet.me>
2026-05-14 20:51:52 +0700

    Only send model footer when message is fully completed (time.completed)

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,5 @@
 export const config = {
   discordToken: process.env.DISCORD_TOKEN || "",
   databasePath: process.env.DATABASE_PATH || "bridge.db",
+  debugEventsPath: process.env.DEBUG_EVENTS_PATH || "",
 };
